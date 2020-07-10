866e02e1f911fb3963eff7f55d47807e456e0e4b
shivani yadav <shivani.yadav@unthinkable.co>
2020-07-10 21:27:30 +0530

    2 nd day regular expression

---
+++ Day7/regular_expressionII.js
@@ -0,0 +1,15 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    
+    
+    /*
+     * Do not remove the return statement
+     */
+    var re=/^(Mr|Mrs|Ms|Dr|Er)(\.)([a-zA-Z])*$/
+
+    return re;
+}
